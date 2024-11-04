ee850b84dddb0c32c2fed5d2459148194c6ca054
Sarankumar <91582966+SarankumarSelvaraj@users.noreply.github.com>
2024-11-04 12:45:39 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,22 +9,26 @@ import express, { NextFunction } from "express";
 import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
-import authRoutes from "./routes/v1/auth/authRouter";
 import { Request, Response } from "express";
 import ApiError from "./utils/apiError";
 import httpStatus from "http-status";
 import { errorMessage } from "./utils/apiMessage";
+import cookieParser from "cookie-parser";
+import router from "./routes/v1";
+
+
 // initiate the app
 const app = express();
 // helps secure your application by setting various HTTP headers to prevent common security vulnerabilities, such as cross-site scripting (XSS), content security policy (CSP), and more  
 app.use(helmet());
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(cors());
 app.options("*", cors());
-app.use("/api/auth", authRoutes);
+app.use("/api/v1", router);
 app.use((req: Request, res: Response, next: NextFunction) => {
   return next(new ApiError(httpStatus.NOT_FOUND, errorMessage[404]));
 });
