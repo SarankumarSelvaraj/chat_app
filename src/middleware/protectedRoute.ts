a29c9c46896ea179f73ccad43aa8fd19385f5208
Sarankumar <91582966+SarankumarSelvaraj@users.noreply.github.com>
2024-11-05 12:14:53 +0530

    Update protectedRoute.ts

--- src/middleware/protectedRoute.ts
+++ src/middleware/protectedRoute.ts
@@ -10,14 +10,12 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { UserPayload } from "../@types/userAuth";
 
-// Define your secret key. In a real application, store this in environment variables.
-const secretKey = process.env.JWT_SECRET || ""; // Replace with your actual secret key
-
 // JWT authentication middleware
-export const protectedRoute = (req: Request, res: Response, next: NextFunction) => {
+export const protectedRoute = (req: Request, res: Response, next: NextFunction): void => {
+  // Define your secret key. In a real application, store this in environment variables.
+  const secretKey = process.env.JWT_SECRET || ""; // Replace with your actual secret key
   // Get the token from the cookies
   const token = req.cookies.jwt; // Assuming the cookie is named 'jwt'
-
   if (token) {
     // Verify the token
     jwt.verify(token, secretKey, (err: any, decoded: any) => {
@@ -25,13 +23,14 @@ export const protectedRoute = (req: Request, res: Response, next: NextFunction) 
         // Token is invalid
         return res.status(401).json({ message: "Unauthorized: Invalid token" });
       }
-
+      console.log(`Verify the data ${JSON.stringify(decoded)}`);
       // Token is valid, attach user info to the request
       req.user = decoded as UserPayload;
       next();
     });
   } else {
+    console.log(`Error in token: ${token}`);
     // No token found
-    return res.status(401).json({ message: "Unauthorized: No token provided" });
+    res.status(401).json({ message: "Unauthorized: No token provided" });
   }
 };
