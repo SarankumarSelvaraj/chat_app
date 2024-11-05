78aa18f44b772902d9bc285118f7f167f4e57635
Sarankumar <91582966+SarankumarSelvaraj@users.noreply.github.com>
2024-11-05 12:16:12 +0530

    Update helper.ts

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -42,7 +42,7 @@ export const generateTokenAndSetCookie = (userId: any, res: Response) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV === "production",
   });
 };
 
