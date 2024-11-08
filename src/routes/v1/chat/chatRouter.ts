6d0030eeaff20b996054c6636287beee796bc8f3
Sarankumar <91582966+SarankumarSelvaraj@users.noreply.github.com>
2024-11-08 15:27:48 +0530

    Update chatRouter.ts

--- src/routes/v1/chat/chatRouter.ts
+++ src/routes/v1/chat/chatRouter.ts
@@ -1,24 +1,28 @@
 /*************************************************
  * Chat app - chat api
- * chatSchema.ts
+ * chatRouter.ts
  * Created by Sarankumar Selvaraj on 01/11/2024
  * Copyright
  *************************************************/
 
-import Joi from "joi";
+import express from "express";
+import END_POINTS from "../../../utils/apiUrl";
+import { validateSchema } from "../../../middleware/validate";
+import chatSchema from "./chatSchema";
+import chatController from "./chatController";
 
-const sendMessage = Joi.object({
-    receiverId: Joi.string().required(),
-    message: Joi.string()
-})
+const chatRoutes = express.Router();
 
-const getMessage = {
-    params: Joi.object().keys({
-        id: String
-    })
-};
+chatRoutes.post(
+    END_POINTS.chat.sendMessage,
+    validateSchema(chatSchema.sendMessage),
+    chatController.sendMessage
+);
 
-export = {
-    sendMessage,
-    getMessage
-};
+chatRoutes.get(
+   END_POINTS.chat.getMessage,
+   validateSchema(chatSchema.getMessage),
+   chatController.getMessage
+);
+
+export default chatRoutes;
